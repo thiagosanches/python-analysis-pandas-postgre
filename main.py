1f89dc3e2fd0fa9cc15775a8a81eda99d7848de1
Thiago Sanches <tsigwt@gmail.com>
2022-07-23 17:29:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-
 import pandas as pd
 import csv
 
+print("Going to read the CSV file...")
 df = pd.read_csv("filtered-data.csv", parse_dates=[["date", "time"]])
+print("Going to read the CSV file... [OK]")
+
+print("Going to groupby and resample...")
 dfByDateTime = df.groupby('request_ip').resample('300S', on="date_time").request_ip.count()
+print("Going to groupby and resample... [OK]")
 
 dfByDateTimeOrdered = dfByDateTime.sort_values('date_time', ascending=False)
 dfByDateTimeOrdered.to_csv('resampled-data.csv')
